b14f05ea604ac1da05a06bf052bae4e63ac5e560
Yulia <yarantseva89@bk.ru>
2023-06-25 17:40:22 +0600

    SEminar Python

---
+++ main.py
@@ -0,0 +1,75 @@
+# print(5, 8, 6)
+
+# n = 1,89
+# print(n)
+
+# n = 'fdfg'
+
+# print(n)
+# n1 = "ndjgnf"
+
+# print(n1)
+
+# n = 'jf"sdffd"gd'
+# print((n))
+
+
+# print(5)
+# print(5)
+# print(5)
+
+# # print(5, 8)
+# # print(5)
+# # print(5, 9)
+
+
+# a = 5
+# b = 5,97
+# c = 'kffg'
+
+# print(f"{a} - {b} - {c}")
+
+# print("{} - {} - {}".format(a, b, c))
+
+# print("веедите строку:")
+# a = int(input())
+# print(a)
+
+# b = int(input('Введите второе число: '))
+                 
+# print(a, '+', b, '=', a+b)
+
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c + '89')
+# print(type(c))
+
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# v = 'dkfjf'
+# v = int(v) // ошибка
+
+
+
+# a = 5.5966
+# b = 6.7657
+# print(round(a*b, 3))
+# round(1.65484, 2)
+
+iter = 2
+iter += 3
+iter -= 4
+iter *= 5
+iter /= 5
+iter //= 5  
+iter %= 5
+iter **= 5
