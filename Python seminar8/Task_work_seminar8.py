6008177b8f5436df36f7419f7e9cb4cc42b2ef9e
Yulia <yarantseva89@bk.ru>
2023-07-18 16:36:51 +0600

    Task seminar 8, HW seminar 8

--- Python seminar8/Task_work_seminar8.py
+++ Python seminar8/Task_work_seminar8.py
@@ -14,185 +14,123 @@
 # 4. Использование функций. Ваша программа
 # не должна быть линейной
 
-# def ShowMenu():
-#     print("\nВыберите необходимое действие:\n"
-#         "1. Открыть справочник\n"
-#         "2. Показать контакты справочника\n"
-#         "3. Добавить контакт\n"
-#         "4. Изменить данные контакта\n"
-#         "5. Удалить контакт\n"
-#         "6. Сохранить справочник в текстовом формате\n"
-#         "7. Закончить работу")
-#     choice = int(input())
-#     return choice
-
-def OpenFile():
-    with open('telPhone.txt', 'r', encoding='utf-8') as file:
-        print(*file)
-    # file = open('telPhone.txt', 'r', encoding='utf-8')
-    # print(*file)
-    # file.close()
-OpenFile()
-
-def OpenListGroup(filename):
-    filename = 'telPhone.txt'
-    with open(filename, 'r', encoding='utf-8') as file:
-    # file = open('telPhone.txt', 'r', encoding='utf-8')
-        worlds = file.readlines()
-    headers = ['Фамилия', 'Имя', 'Отчество', 'Номер телефона']
-    telDirect = []
-    for line in worlds:
-        line = line.strip().split()
-        telDirect.append(dict(zip(headers, line)))
-    return telDirect
-print(OpenListGroup('telPhone.txt'))
-print()
-
-def PrintForKey(telDirect: list):
-    for contact in telDirect:
-        for key, value in contact.items:
-            print(f'{key}: {value}', end=";")
-        # print()
-
-def ShowPhonebook(filename):
-    contacts = sorted(OpenListGroup(filename), key=lambda x: x['Фамилия'])
-    PrintForKey(contacts)
-    return contacts
-# ShowPhonebook('telPhone.txt')
-        
+def OpenFile(file): #открытие и чтение файла
+    with open(file, 'r', encoding='utf-8') as phonebook:
+        contact = phonebook.readlines()
+        for line in contact:
+            print(line)
+    # print(contact, end=";")
 
 
+def DirTelBook(file): #преобразование в список словорей
+    with open(file, 'r', encoding='utf-8') as phonebook:
+        contact = phonebook.readlines()
+    names = ['Фамилия', 'Имя', 'Отчество', 'Номер телефона']
+    tel = []
+    for line in contact:
+        line = line.strip().split(', ')
+        tel.append(dict(zip(names, line)))
+    for i in (tel):
+        print(i, end='\n')
+    print(type(tel))
+    return tel
+
+def NewContakt (): #ввод нового контакта пользователем
+    print("Введите Фамилию")
+    firsName = input()
+    print("Введите Имя")
+    secondName = input()
+    print("Введите отчество")
+    treeName = input()
+    print("Введите номер телефона")
+    phone = input()
+    return firsName, secondName, treeName, phone
+
+def AddingData(file): #добавление нового контакта в телефонную книгу
+    newContact = ', '.join(NewContakt())
+    with open(file, 'a+', encoding='utf-8') as phonebook:
+        phonebook.writelines(newContact + '\n')
+
+def SearchByCriteria(): #ввод криетериев поиск контактов 
+    print("Введите параметр поика\n 1 - Фамилия\n 2 - Имя\n 3 - Отчество\n 4 - Номер телефона")
+    serchBySet = input()
+    setVariant = None
+    if serchBySet == '1':
+        print("Ведите Фамилию")
+        setVariant = input()
+    elif serchBySet == '2':
+        print("Ведите Имя")
+        setVariant = input()
+    elif serchBySet == '3':
+        print("Ведите Номер телефона")
+        setVariant = input()
+    return serchBySet, setVariant
+
+def SearchContackt(tel, search): #поиск и печать контакта по выбраному параметру
+    serchBySet, setVariant = SearchByCriteria()
+    contakt = []
+    for cont in tel:
+        if cont[search[serchBySet]] == setVariant:
+            contakt.append(cont)
+    for i in contakt:
+        print(i)
+        print(type(i))
+    print(contakt)
+    print(type(contakt))
+
+def СhangeСontact(file, tel, search): #замена выбраных параметров
+    serchBySet, setVariant = SearchByCriteria()
+    contakt = []
+    contPhone = []
+    for cont in tel:
+        if cont[search[serchBySet]] == setVariant:
+            contakt.append(cont)
+    print(contakt)
+    for i in contakt:
+        if i.values() == setVariant:
+            contPhone.append(contakt)
+    print(contPhone)
+    with open(file, 'r+', encoding='utf-8') as phonebook:
+        for cont in phonebook:
+            line = ' '.join(i) + '\n'
+            phonebook.write(line)
+
+def DelitContact(tel,file, search): #удаление контакта
+    serchBySet, setVariant = SearchByCriteria()
+    with open(file, 'w+', encoding='utf-8') as phonebook:
+        for cont in tel:
+            if cont[search[serchBySet]] == setVariant:  
+                line = ' '.join(cont) + '\n'
+            phonebook.write(line)
 
 
-
-
-# def ReadFile(filename):
-#     filename = 'telPhone.txt'
-#     with open(filename, 'r', encoding='utf-8') as file:
-#         lines = file.readlines()
-#     headers = ['Фамилия', 'Имя', 'Отчество', 'Номер телефона']
-#     contactList = []
-#     for line in lines:
-#         line = line.strip().split()
-#         contactList.append(dict(zip(headers, line)))
-#     return contactList
-# print(ReadFile('telPhone.txt'))
-
-# def PrintContacts(contactList: list):
-#     for contact in contactList:
-#         for key, value in contact.items():
-#             print(f'{key}: {value}', end='')
-#         print()
-
-# def ShowPhonebook(filename):
-#     listOfContacts = sorted(ReadFile(filename), key=lambda x: x['Фамилия'])
-#     PrintContacts(listOfContacts)
-#     return listOfContacts
-
-# def SearchParameters():
-#     print('Поиск по полю')
-#     searchField = input('1 - по фамилии\n2 - по имени\n3 - по отчеству\n4 - по номеру телефона\n')
-#     print()
-#     searchValue = None
-#     if searchField == '1':
-#         searchValue = input('Введите фамилию для поиска: ')
-#         print()
-#     elif searchField == '2':
-#         searchValue = input('Введите имя для поиска: ')
-#         print()
-#     elif searchField == '3':
-#         searchValue = input('Введите отчество для поиска: ')
-#         print()
-#     elif searchField == '4':
-#         searchValue = input('Введите номер телефона для поиска: ')
-#         print()
-#     return searchField, searchValue
-
-# def FindNumber(contactList):
-#     searchField, searchValue = SearchParameters()
-#     searchValueDict = {'1': 'Фамилия', '2': 'Имя', '3': 'Отчество', '4': 'Номер телефона'}
-#     foundContacts = []
-#     for contact in contactList:
-#         if contact[searchValueDict[searchField]] == searchValue:
-#             foundContacts.append(contact)
-#     if len(foundContacts) == 0:
-#         print('Контакт не найден!')
-#     else:
-#         PrintContacts(foundContacts)
-
-# def GetNewNumber():
-#     lastName = input('Введите фамилию: ')
-#     firstName = input('Введите имя: ')
-#     phoneNumber = input('Введите номер телефона: ')
-#     return lastName, firstName, phoneNumber
-
-
-# def AddPhoneNumber(filename):
-#     info = ' '.join(GetNewNumber())
-#     with open(filename, 'a', encoding='utf-8') as file:
-#         file.write(f'{info}\n')
-
-# def ChangePhoneNumber(filename):
-#     contactList = ReadFile(filename)
-#     numberToChange = SearchToModify(contactList)
-#     contactList.remove(numberToChange)
-#     print('Какое поле вы хотите изменить?')
-#     field = input('1 - Фамилия\n2 - Имя\n3 - Номер телефона\n')
-#     if field == '1':
-#         numberToChange[0] = input('Введите фамилию: ')
-#     elif field == '2':
-#         numberToChange[1] = input('Введите имя: ')
-#     elif field == '3':
-#         numberToChange[2] = input('Введите номер телефона: ')
-#     contactList.append(numberToChange)
-#     with open(filename, 'w', encoding='utf-8') as file:
-#         for contact in contactList:
-#             line = ' '.join(contact) + '\n'
-#             file.write(line)
-
-# def SearchToModify(contactList: list):
-#     searchField, searchValue = SearchParameters()
-#     searchResult = []
-#     for contact in contactList:
-#         if contact[int(searchField) - 1] == searchValue:
-#             searchResult.append(contact)
-#     if len(searchResult) == 1:
-#         return searchResult[0]
-#     elif len(searchResult) > 1:
-#         print('Найдено несколько контактов')
-#         for i in range(len(searchResult)):
-#             print(f'{i + 1} - {searchResult[i]}')
-#         numberCount = int(input('Выберите номер контакта, который нужно изменить/удалить: '))
-#         return searchResult[numberCount - 1]
-#     else:
-#         print('Контакт не найден')
-#     print()
-
-# def DeleteContact(filename):
-#     contactList = ReadFile(filename)
-#     numberToChange = SearchToModify(contactList)
-#     contactList.remove(numberToChange)
-#     with open(filename, 'w', encoding='utf-8') as file:
-#         for contact in contactList:
-#             line = ' '.join(contact) + '\n'
-#             file.write(line)
+def ShowMenu():
+    print("\nВыберите необходимое действие:\n"
+        "1. Открытие и чтение файла\n"
+        "2. Добавление нового контакта в телефонную книгу\n"
+        "3. Поиск и печать контакта по выбраному параметру\n"
+        "4. Замена выбраных параметров\n"
+        "5. Удаление контакта\n"
+        "6. Закончить работу")
+    choice = int(input())
+    return choice
 
 def WorkWithPhonebook():
     choice = ShowMenu()
-    phonebook ='telPhone.txt'
-    phonebookRead =  ReadFile('telPhone.txt')
-    while (choice != 7):
+    file = 'telPhone.txt'
+    tel = DirTelBook(file)
+    search = {'1': 'Фамилия', '2': 'Имя', '3': 'Отчество', '4': 'Номер телефона'}
+    while (choice != 6):
         if choice == 1:
-            OpenFile(phonebook)
+            OpenFile(file)
         elif choice == 2:
-            telDirect = OpenListGroup(phonebookRead)
-            FindNumber(telDirect)
+            AddingData(file)
         elif choice == 3:
-            AddPhoneNumber(phonebook)
+            SearchContackt(tel, search) 
         elif choice == 4:
-            ChangePhoneNumber(phonebook)
+            СhangeСontact(file, tel, search)
         elif choice == 5:
-            DeleteContact(phonebook)
+            DelitContact(tel, file, search)
         choice = ShowMenu()
 
-# WorkWithPhonebook()    
+WorkWithPhonebook()   
